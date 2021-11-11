356ac4dc461b24140d10460bafab4817003987be
Hubbotu <34053496+Hubbotu@users.noreply.github.com>
2021-11-11 07:04:38 +0300

    Update Locales.lua

--- Locales.lua
+++ Locales.lua
@@ -17,8 +17,10 @@ if locale == "zhCN" then
 	L["Macro %s already exists, please enter another name."] = "宏 %s 已存在, 请输入其他名称。"
 	L["Macro %s has been renamed to %s."] = "宏 %s 更名为 %s。"
 	L["The number of General Macros has reached the upper limit."] = "通用宏数量已达上限。"
+	L["Macro command exceeds 255 characters"] = "宏命令超出255个字符"
 end
 
+-- ruRU by ZamestoTV
 if locale == "ruRU" then
 	L["Signature Ability"] = "Общая способность"
 	L["Class Ability"] = "Классовая способность"
@@ -34,9 +36,11 @@ if locale == "ruRU" then
 	L["Macro %s already exists, please enter another name."] = "Макрос %s уже существует. Введите другое название."
 	L["Macro %s has been renamed to %s."] = "Макрос %s был переименован в %s."
 	L["The number of General Macros has reached the upper limit."] = "Количество общих макросов достигло предела."
+	L["Macro command exceeds 255 characters"] = "Команда макроса превышает 255 символов"
 end
 
---[[ if locale == "enUS" then
+--[[ -- example
+if locale == "enUS" then
 	L["Signature Ability"] = "Signature Ability"
 	L["Class Ability"] = "Class Ability"
 	L["Macro Name"] = "Macro Name"
@@ -51,4 +55,5 @@ end
 	L["Macro %s already exists, please enter another name."] = "Macro %s already exists, please enter another name."
 	L["Macro %s has been renamed to %s."] = "Macro %s has been renamed to %s."
 	L["The number of General Macros has reached the upper limit."] = "The number of General Macros has reached the upper limit."
+	L["Macro command exceeds 255 characters"] = "Macro command exceeds 255 characters"
 end ]]
